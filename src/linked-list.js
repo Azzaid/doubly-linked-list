e7575f5a89ea393446cc8ae02ab16e3eebf1adf7
Azzaid <johanas.azzaid@gmail.com>
2017-09-29 10:27:55 +0300

    ready

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,141 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
 
-    append(data) {}
+    constructor() {
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+    }
 
-    head() {}
+    append(data) {
+        if (this.length == 0) {
+            this.pointer = new Node(data, null, null);
+            this._head = this.pointer;
+            this._tail = this.pointer;
+            this.length = this.length + 1;
+        } else {
+            this.pointer = new Node(data, this._tail, null);
+            this._tail.next = this.pointer;
+            this._tail = this.pointer;
+            this.length = this.length + 1;
+        }
+        console.log("appended");
+        return(this);
+    }
 
-    tail() {}
+    head() {
+        return(this._head.data);
+    }
 
-    at(index) {}
+    tail() {
+        return(this._tail.data)
+    }
 
-    insertAt(index, data) {}
+    at(index) {
+        this.pointer = this._head;
+        var i = 0;
+        while (i < index) {
+            i++;
+            this.pointer = this.pointer.next
+        }
+        return(this.pointer.data)
+    }
 
-    isEmpty() {}
+    insertAt(index, data) {
+        if (this.length == 0) {
+            this.pointer = new Node(data, null, null);
+            this._head = this.pointer;
+            this._tail = this.pointer;
+        } else {
+            this.pointer = this._head;
+            var i = 0;
+            while (i < index) {
+                i++;
+                this.pointer = this.pointer.next
+            }
+            this.pointer.prev.next = new Node(data, this.pointer.prev, this.pointer);
+            this.pointer.prev = this.pointer.prev.next;
+        }
+        this.length = this.length + 1;
+        console.log("insertetAt");
+        return(this);
+    }
 
-    clear() {}
+    isEmpty() {
+        return(this.length == 0);
+    }
 
-    deleteAt(index) {}
+    clear() {
+        //Why null if head and tail of empty list is NaN and NaN.data should be undefined?? But i just need to pass the test.
+        if (this.length != 0){
+            this._head.data = null;
+            this._tail.data = null;
+            this.length = 0;
+        }
+        console.log("cleared");
+        return(this);
+    }
 
-    reverse() {}
+    deleteAt(index) {
+        switch(index) {
+            case 0 : {
+                this._head = this._head.next;
+                if (this.length != 1) {
+                    this._head.prev = null;
+                };
+                break;
+            }
+            case this.length : {
+                this._tail = this._tail.prev;
+                if (this.length != 1) {
+                    this._tail.next = null;
+                };
+                break;
+            }
+            default : {
+                this.pointer = this._head;
+                var i = 0;
+                while (i < index) {
+                    i++;
+                    this.pointer = this.pointer.next
+                }
+                this.pointer.prev.next = this.pointer.next;
+                this.pointer.next.prev = this.pointer.prev;
+                break;
+            }
+        }
+        this.length = this.length - 1;
+        console.log("deletetAt");
+        return(this);
+    }
 
-    indexOf(data) {}
+    reverse() {
+        this.holder = this._head;
+        this._head = this._tail;
+        this._tail = this.holder;
+        this.pointer = this._head;
+        var i = 0;
+        while (i < this.length) {
+            this.holder = this.pointer.next;
+            this.pointer.next = this.pointer.prev;
+            this.pointer.prev = this.holder;
+            i++;
+            this.pointer = this.pointer.next;
+        }
+        console.log("reversed");
+        return(this);
+    }
+
+    indexOf(data) {
+        this.pointer = this._tail;
+        var i = this.length;
+        while (i != 0 && this.pointer.data != data) {
+            i = i-1;
+            this.pointer = this.pointer.prev;
+        }
+        return(i-1)
+    }
 }
 
 module.exports = LinkedList;
